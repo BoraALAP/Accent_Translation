687e42343d3e4b5d8d9e7c4e8a9f20f2d3521ac0
Arttic Fox <bora.alap@artticfox.com>
2018-11-18 15:21:30 -0500

    homepage animations

---
+++ dev/script/main.js
@@ -0,0 +1,154 @@
+const app = {};
+
+app.events = () => {
+	const menuBut = document.getElementById('hamburger');
+	
+	menuBut.addEventListener('click',(e) => {
+		const menuUl = document.getElementById('menuUl');
+		const menuSVG = menuBut.querySelector('svg');
+		menuBut.classList.toggle('active');
+		menuUl.classList.toggle('active');
+
+		if (menuBut.classList.contains('active')){
+			menuSVG.dataset.icon = 'times'
+		} else {
+			menuSVG.dataset.icon = 'bars'
+		}
+	});
+}
+
+app.counters = () => {
+	const numbers = [].slice.call(document.querySelectorAll('.counterNum'));
+	numbers.forEach((item) => {
+		const max = item.dataset.to;
+		let min = 0;
+		let timing = 100
+
+		if (max > 3000){
+			timing = 1 / 100000;
+		}
+
+		console.log(max % 20)
+
+	  // return new Promise(resolve => {
+		const timer = setInterval(() => {
+			if (min < max){
+				if (max > 3000){
+				  	min = min + 10;
+				} else {
+			    	min++;
+			    }
+			}
+			item.innerText = min;
+			if (min == max){
+				clearInterval(timer);
+			}
+		}, timing)  
+	})
+}
+
+app.testimonials = () => {
+
+	let array = []
+
+	fetch('./public/js/testimonials.json')
+	.then((res) => {
+		return res.json();
+	})
+	.then(res => {
+		displayTesti(res.testimonials)
+	})
+	.catch(err => {
+		console.log(err)
+	})
+
+		
+	const displayTesti = (array) => {
+		let counter = 0;
+		const container = document.querySelector('.testimonial');
+		const testi = (array, counter) => {
+			
+			const testNum = document.querySelector('#testimonialNum');
+			container.innerHTML = `
+			<p><span class="text">"${array[counter].testimonial}"</span></p>
+			<span class="name"><i>${array[counter].person}</i></span>`;
+
+			testNum.innerHTML = `${counter +1}/${array.length}`;
+		}
+		const arrows = [].slice.call(document.querySelectorAll('.testimonialArrows button'));
+		arrows.forEach((item) => {
+			item.addEventListener('click',() => {
+				return new Promise ((resolve,reject) => {
+					container.classList.add('transition');
+					setTimeout(() => {		
+						if (item.getAttribute('data-direction') == 'right'){
+							if(counter < array.length -1) {
+								counter ++;
+							} else {
+								counter = 0;
+							}
+					  	} else {
+					  		if(counter > 0) {
+								counter --;
+							} else {
+								counter = array.length -1;
+							}
+					  	}
+					  	resolve(counter);
+					}, 1000)				
+				}).then(resolve =>{
+					testi(array,counter);
+					container.classList.remove('transition');
+					console.log(resolve)
+				})
+			});
+		})
+		testi(array,counter);
+	}
+}
+
+app.inView = () => {
+	// document.getElementById('counters').classList.add('in-view');
+
+	const isScrolledIntoView = (elem) => {
+	    var docViewTop = window.scrollTop;
+	    var docViewBottom = docViewTop + window.innerHeight;
+
+	    var elemTop = elem.offsetTop + 200;
+	    var elemBottom = elemTop;
+	    //  + $(elem).height() - 200
+
+	    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+	}
+
+	var something = (function() {
+			    var executed = false;
+			    return function() {
+			        if (!executed) {
+			            executed = true;
+			            app.counters();
+			        }
+			    };
+			})();
+
+	window.addEventListener('scroll', (e) => {
+	  const counters = [].slice.call(document.querySelectorAll('.counterHome')).forEach((item) => {
+	        
+	        if ((window.pageYOffset + window.innerHeight - 300) >= item.offsetTop) {
+	            item.classList.add('in-view');
+	            something();
+	        } else {
+	        	item.classList.remove('in-view');
+	        } 
+	    });
+	}); 
+
+}
+
+app.init = () => {
+	app.testimonials();
+	app.inView();
+	app.events();
+}
+
+app.init();
