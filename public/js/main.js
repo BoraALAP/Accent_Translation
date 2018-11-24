ac6e6106500f6c6e348e6b7b8540ca4fe9a03b8f
Arttic Fox <bora.alap@artticfox.com>
2018-11-23 19:16:22 -0500

    homepage, contactus page, immigration applicants pages are pretty much placed.

--- public/js/main.js
+++ public/js/main.js
@@ -80,8 +80,6 @@
 					timing = 1 / 100000;
 				}
 
-				console.log(max % 20);
-
 				// return new Promise(resolve => {
 				const timer = setInterval(() => {
 					if (min < max) {
@@ -169,9 +167,9 @@
 				return elemBottom <= docViewBottom && elemTop >= docViewTop;
 			};
 
-			var something = function () {
-				var executed = false;
-				return function () {
+			const something = function () {
+				let executed = false;
+				return () => {
 					if (!executed) {
 						executed = true;
 						app.counters();
@@ -192,8 +190,59 @@
 			});
 		};
 
+		app.languageSelector = () => {
+			const languagesBtn = [].slice.call(document.querySelectorAll('#languages li .btn'));
+
+			const fetching = languageSelect => {
+				fetch(`./public/js/languageText.json`).then(res => res.json()).then(res => {
+					markupWhyUs(res, languageSelect);
+				}).catch(err => {
+					console.log(err);
+				});
+			};
+
+			languagesBtn.forEach(language => {
+				const flag = language.querySelector('img');
+				flag.setAttribute("src", `/assets/svg/flag/${language.innerText.toLowerCase()}.svg`);
+
+				language.addEventListener('click', e => {
+					let languageName = language.textContent.toLowerCase();
+					// language.querySelector('button')
+					languagesBtn.forEach(languageSib => {
+						languageSib.classList.remove('active');
+					});
+					language.classList.add('active');
+
+					fetching(languageName);
+				});
+			});
+
+			const markupWhyUs = (text, language) => {
+				const container = document.getElementById('why-us');
+				const res = text[language];
+
+				const generateText = data => {
+					let result = [];
+					for (let line in data) {
+						result.push(`<div><h6>${data[line].title}</h6><p>${data[line].text}</p></div>`);
+					}
+
+					return result.join('');
+				};
+
+				container.innerHTML = `
+		<div class="container">
+			<h5>Accent Translations is:</h5>
+			${generateText(res.whyus)}
+		</div>`;
+			};
+
+			fetching("english");
+		};
+
 		app.init = () => {
 			app.testimonials();
+			app.languageSelector();
 			app.inView();
 			app.events();
 		};
