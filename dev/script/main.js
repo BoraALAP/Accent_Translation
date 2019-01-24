a216afadc3659ac5462fd705d7c1406cc9647945
Arttic Fox <bora.alap@artticfox.com>
2019-01-23 21:13:37 -0500

    why us page update

--- dev/script/main.js
+++ dev/script/main.js
@@ -8,45 +8,6 @@ app.events = () => {
 	})
 }
 
-app.counters = () => {
-	const numbers = [].slice.call(document.querySelectorAll('.counterNum'));
-	const comma = document.querySelector(".counterNum[data-type='comma']");
-	console.log(comma.dataset.to / 10);
-	
-	numbers.forEach((item) => {
-		let max = item.dataset.to;
-		let min = 0; 
-		let timing;
-
-		console.log(max);
-		
-
-		if (max > 3000){
-			timing = item.dataset.to / 1200;
-		} else if (item.hasAttribute('data-type')) {	
-			max = item.dataset.to/10;		
-			timing = item.dataset.to / 10;
-		} else {
-			max = item.dataset.to;
-			timing = 10;
-		}
-
-		const timer = setInterval(() => {
-			if (min < max) {
-				min = min + (max / 1000);
-			}
-			if (item.hasAttribute('data-type')){
-				item.innerText = min.toFixed(1);
-			} else {
-				item.innerText = Math.round(min);
-			}
-			if (min >= max) {
-				clearInterval(timer);
-			}
-		}, timing)
-	})
-}
-
 app.testimonials = () => {
 
 	let array = []
@@ -107,6 +68,41 @@ app.testimonials = () => {
 	}
 }
 
+app.counters = () => {
+	const numbers = [].slice.call(document.querySelectorAll('.counterNum'));
+	const comma = document.querySelector(".counterNum[data-type='comma']");
+	
+	numbers.forEach((item) => {
+		let max = item.dataset.to;
+		let min = 0; 
+		let timing;
+
+		if (max > 3000){
+			timing = item.dataset.to / 1200;
+		} else if (item.hasAttribute('data-type')) {	
+			max = item.dataset.to/10;		
+			timing = item.dataset.to / 10;
+		} else {
+			max = item.dataset.to;
+			timing = 10;
+		}
+
+		const timer = setInterval(() => {
+			if (min < max) {
+				min = min + (max / 1000);
+			}
+			if (item.hasAttribute('data-type')){
+				item.innerText = min.toFixed(1);
+			} else {
+				item.innerText = Math.round(min);
+			}
+			if (min >= max) {
+				clearInterval(timer);
+			}
+		}, timing)
+	})
+}
+
 app.inView = () => {
 	// document.getElementById('counters').classList.add('in-view');
 
@@ -145,6 +141,7 @@ app.inView = () => {
 }
 
 app.languageSelector = () => {
+	
 	const languagesBtn = [].slice.call(document.querySelectorAll('#languages li .btn'));
 
 
@@ -199,88 +196,13 @@ app.languageSelector = () => {
 	fetching("english");
 }
 
-app.applicantForm = () => {
-	var $form = $('form#test-form'),
-		url = 'https://script.google.com/macros/s/AKfycbxV-sS5obCWcSxQx6Mzc2Rln0dTDHWLPCUcy_APR1Emmuq1Iwbe/exec'
 
-
-	$('#submit-form').on('click', function (e) {
-		e.preventDefault();
-
-		// fetch(url, {
-		//   method: "GET",
-		//   mode: 'cors',
-		//   data: $form.serialize()
-		// }).then(res => {
-		// 	return res.json();
-		// 	console.log(res)
-		// }).catch(err => {
-		// 	console.log(err)
-		// })
-
-		var jqxhr = $.ajax({
-			url: url,
-			method: "GET",
-			dataType: "json",
-			data: $form.serialize()
-		}).done((res) => {
-			console.log(res)
-		})
-
-		var file,
-			reader = new FileReader();
-
-
-
-		function showSuccess(e) {
-			if (e === "OK") {
-				$('#forminner').hide();
-				$('#success').show();
-			} else {
-				showError(e);
-			}
-		}
-
-
-		var files = $('#file')[0].files;
-
-		if (files.length === 0) {
-			showError("Please select a file to upload");
-			return;
-		}
-
-		file = files[0];
-
-		if (file.size > 1024 * 1024 * 5) {
-			showError("The file size should be < 5 MB. Please <a href='http://www.labnol.org/internet/file-upload-google-forms/29170/' target='_blank'>upgrade to premium</a> for receiving larger files in Google Drive");
-			return;
-		}
-
-		showMessage("Uploading file..");
-
-		reader.readAsDataURL(file);
-
-
-
-		function showError(e) {
-			$('#progress').addClass('red-text').html(e);
-		}
-
-		function showMessage(e) {
-			$('#progress').removeClass('red-text').html(e);
-		}
-
-
-	})
-}
 
 app.hoverLetter = () => {
 	const letters = document.querySelectorAll('#letters>h3');
 	const theWord = document.getElementById('the_word');
 	
 	let currentLetter = 0;
-	
-	console.log(letters);
 	
 	setInterval( function() {
 		theWord.innerText = letters[currentLetter].getAttribute('data-name');
@@ -294,12 +216,17 @@ app.hoverLetter = () => {
 }
 
 app.init = () => {
-	app.testimonials();
-	app.languageSelector();
-	app.inView();
+	if(window.location.pathname == "/"){ 
+		app.testimonials();
+		app.inView();
+	}
+	if(window.location.pathname == "/imm-applicants.html"){ 
+		app.languageSelector();
+	}
 	app.events();
-	app.applicantForm();
-	app.hoverLetter();
+	if(window.location.pathname == "/why-us.html"){ 
+		app.hoverLetter();
+	}
 }
 
 app.init();
